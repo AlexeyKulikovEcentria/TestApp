f9cbe099bec02985f0ff143f7649397fb921f0c9
AlexeyKulikovEcentria <107705861+AlexeyKulikovEcentria@users.noreply.github.com>
2022-06-17 11:29:15 -0500

    Update CsvFileReader.cs

--- CSVFileReader/CsvFileReader.cs
+++ CSVFileReader/CsvFileReader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,12 +7,24 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
 
+// review.
+// code is not structured properly
+// everything in the single file.
+
 namespace CSVFileReader
 {
     public class CsvFileReader : ICsvFileReader
     {
+        // review.
+        // separation of concerns violated
+        // the reader delegates the reading operation to CsvFile class
+
         public Task<ICsvFile> ReadFileAsync(string fileName)
         {
+            // review.
+            // the async file reading is recommended here
+            // see: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/using-async-for-file-access
+            
             Task<ICsvFile> task = new Task<ICsvFile>(() => new CsvFile(fileName));
             return task;
         }
@@ -28,9 +40,18 @@ namespace CSVFileReader
             LoadFile(fileName);
         }
 
+        // review.
+        // this constructor is never used and makes no sense
+
         public CsvFile() { }
 
         #region Get Methods
+
+        // review.
+        // better to use Enum for any kind of Status flags
+        // or even switch to Exceptions approach (program may throw exception if error happened)
+        // also the FileStatus is not available from ICsvFile interface
+
         public string FileStatus { get; set; }
         public ICsvRow this[int rowIndex]
         {
@@ -67,10 +88,20 @@ namespace CSVFileReader
         }
         #endregion
 
+        // review.
+        // separation of concerns violated
+        // see above (comment on CsvFileReader class)
+        // no necessary to set this method as Public
+
         public void LoadFile(string fileName)
         {
             if (File.Exists(fileName))
             {
+                // review.
+                // there is no handling of multi line values
+                // see https://datatracker.ietf.org/doc/html/rfc4180
+                // pt. 2.6 "Fields containing line breaks (CRLF), double quotes, and commas should be enclosed in double-quotes"
+
                 string[] rawRows = File.ReadAllLines(fileName);
                 //Sanity check
                 if (rawRows.Length > 0)
@@ -97,6 +128,12 @@ namespace CSVFileReader
                                     }
                                     else
                                     {
+                                        // review.
+                                        // by the spec. https://datatracker.ietf.org/doc/html/rfc4180
+                                        // pt 2.4
+                                        // Each line should contain the same number of fields throughout the file
+                                        // better to raise a data format error
+
                                         //If the row has less columns then the header row add a blank field to the row
                                         row.AddField(_Columns[x], "");
                                     }
@@ -107,6 +144,10 @@ namespace CSVFileReader
 
                         FileStatus = "File has been loaded.";
                     }
+
+                    // review.
+                    // else { ...
+                    // In this case the status will be null
                 }
             }
             else
@@ -117,16 +158,24 @@ namespace CSVFileReader
 
         private void SetColumnHeader(string[] headerRow)
         {
+            // review.
+            // O(N) linear complexity
+            // better to use other data structure
+
             List<string> header = new List<string>();
 
             foreach (string column in headerRow)
             {
+                // review.
+                // Invalid implementation, the Task has additional requirement: Columns cannot duplicate
+                // The program could raise an error here
+
                 if (header.Contains(column))
                 {
                     //If the header is duplicated attempt to make it unique before adding it.
                     header.Add(column + header.Count(x => x == column).ToString());
                 }
-                else if(string.IsNullOrEmpty(column.Trim()))
+                else if (string.IsNullOrEmpty(column.Trim()))
                 {
                     //If there was a blank in the header
                     header.Add("Blank Column" + header.Count(x => x == "Blank Column").ToString());
@@ -140,6 +189,14 @@ namespace CSVFileReader
 
         }
 
+        // review.
+        // 1) no proper handling of quotes in the value
+        // spec. https://datatracker.ietf.org/doc/html/rfc4180
+        // pt. 2.7
+        // example: "col1,col 2,"this is "" tricky"
+        // 2) no proper handling of data format errors
+        // 3) regular expression are pretty slow, better to use simple loop
+
         public string[] CsvSplitLine(string line)
         {
             Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
@@ -147,6 +204,9 @@ namespace CSVFileReader
             CleanRow(ref returnColumns);
             return returnColumns;
         }
+
+        // review.
+        // ref keyword is not needed here
 
         /// <summary>
         /// Removes double quotes from each column of the given row
@@ -156,6 +216,12 @@ namespace CSVFileReader
         {
             for (int i = 0; i < rawRow.Length; i++)
             {
+
+                // review.
+                // unnecessary trimming
+                // spec. https://datatracker.ietf.org/doc/html/rfc4180
+                // pt. 2.4 Spaces are considered part of a field and should not be ignored.
+
                 string field = rawRow[i].Trim();
                 if (field.Length >= 2)
                 {
@@ -173,6 +239,9 @@ namespace CSVFileReader
     }
     public class CsvRow : ICsvRow
     {
+        // review.
+        // Not needed to store Names
+        // better to have a different data structure
 
         private List<(string ColumnName, string Value)> _Columns =  new List<(string, string)>();
 
@@ -187,6 +256,11 @@ namespace CSVFileReader
             _Columns.Add((columnName.ToLower(), value));
         }
 
+        // review.
+        // non optimal solution for files with a large amount of columns
+        // O(N) complexity for each value getting operation
+        // better choose other data structure
+
         public string this[string columnName]
         {
             get
@@ -199,6 +273,9 @@ namespace CSVFileReader
                 }
                 else
                 {
+                    // review.
+                    // how can we discriminate if this is an Error or the value "Invalid Column"?
+             
                     return "Invalid Column";
                 }
             }
